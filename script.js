9825126672386af17204591a448566589c03b8a3
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-07-15 00:29:58 -0400

    updated js and README.md

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
 var today = moment().format("dddd, MMMM Do");
-$("#currentDay").append(today);
+$("#currentDay").text(today);
 
+var currentHour = moment().format("HH");
+var currentHourParse = parseInt(currentHour);
+console.log(currentHourParse);
 
-var auditTime = function() {
-//    console.log(checkTime);
+$("#hour9").attr("data-time", moment("9AM", "hA").format("HH"));
+$("#hour10").attr("data-time", moment("10AM", "hA").format("HH"));
+$("#hour11").attr("data-time", moment("11AM", "hA").format("HH"));
+$("#hour12").attr("data-time", moment("12PM", "hA").format("HH"));
+$("#hour1").attr("data-time", moment("1PM", "hA").format("HH"));
+$("#hour2").attr("data-time", moment("2PM", "hA").format("HH"));
+$("#hour3").attr("data-time", moment("3PM", "hA").format("HH"));
+$("#hour4").attr("data-time", moment("4PM", "hA").format("HH"));
+$("#hour5").attr("data-time", moment("5PM", "hA").format("HH"));
 
-var i = 0;
-
-//var formHour = $.trim($("label").text());
-var formHour = $( "label" ).each(function( index ) {
-    
-    var newFormHour = $.trim(document.getElementsByTagName('label')[i].firstChild.data);
-    
-    //console.log(newFormHour);
-
-//formHour = moment().format("HH");
-
-var d = new Date();
-var n = 10; //d.getHours();
-console.log(n);
-
-var convertedFormHour = moment(newFormHour, ["h A"]).format("HH");
-//moment().format('[today] HH');
-console.log(convertedFormHour);
-
-if (convertedFormHour > n) { 
-    $(".form-control:eq(i)").addClass(".future bg-success");
-    console.log("success");
-}
-if (convertedFormHour < n) { 
-    $(".form-control:eq(i)").addClass(".past bg-dark");
-    console.log("light");
+var hours = function() {
+    for (var counter = 1; counter <= 12; counter++) {
+        $("#" + counter + "Row").val(localStorage.getItem(counter));
     }
-if (convertedFormHour == n) {
-    $(".form-control:eq(i)").addClass(".present bg-danger");
-    console.log("danger");
 };
 
-    i++;
+var auditTime = function() {
 
-});
-//console.log("working");
-}
+    hours();
+
+    for (var i = 1; i <= 12; i++) {
+        var formTime = $("#" + i + "Row").attr("data-time");
+        var formTimeParse = parseInt(formTime);
+        
+        if (currentHourParse === formTimeParse){
+            $("#" + i + "Row").addClass("present");    
+        }
+        if (currentHourParse > formTimeParse){
+            $("#" + i + "Row").addClass("past");
+        }
+        if (currentHourParse < formTimeParse){
+            $("#" + i + "Row").addClass("future");
+        }
+    }  
+ };
 
 auditTime();
+
+$(".saveBtn").click(function() {
+    
+    var formHour = $(this).attr("data-hour");
+    var formTextarea = $("#" + formHour + "Row").val();
+
+    localStorage.setItem(formHour, formTextarea);
+    
+});
